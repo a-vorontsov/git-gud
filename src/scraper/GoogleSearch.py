67613a318ee4450874627b1594322f2253026a42
paumue <paulhandball@gmail.com>
2019-03-30 19:33:28 +0100

    google search fix

--- src/scraper/GoogleSearch.py
+++ src/scraper/GoogleSearch.py
@@ -1,29 +1,28 @@
 from googlesearch import search
 import re
-import bs4
 import requests
+
+f = open('helloworld.html','w')
 
 def find_questions(git_error):
     results = []
 
     for url in search(git_error, stop=20):
         if "stackoverflow" in url:
-            print(url)
             m = re.search('questions/(.+?)/', url)
+            print(url)
             if m:
                 number = m.group(1)
             results.append(number)
-
     return results
 
 def get_answer(number):
-    r = requests.get('https://api.stackexchange.com/2.2/answers?key=U4DMV*8nvpm3EOpvf69Rxw(('
-                     '&site=stackoverflow&page=%s&pagesize=1&order=desc&sort=votes&filter=!-*jbN.L_SaIL' % number)
+    r = requests.get("https://api.stackexchange.com/2.2/questions/%s/answers?key=U4DMV*8nvpm3EOpvf69Rxw((&site=stackoverflow&page=1&pagesize=1&order=desc&sort=votes&filter=!-*jbN.L_SaIL" % number)
     return r.json()
 
+f.write(get_answer(find_questions("git fetch")[0]).get('items')[0].get('body'))
+f.close()
 
 
-print(get_answer(find_questions("git fetch")[0]))
 
-
-#'https://api.stackexchange.com/2.2/answers?key=U4DMV*8nvpm3EOpvf69Rxw((&site=stackoverflow&page=1&pagesize=1&order=desc&sort=votes&filter=!-*jbN.L_SaIL'
+#'https://api.stackexchange.com/2.2/answers?key=U4DMV*8nvpm3EOpvf69Rxw((&site=stackoverflow&page=11644858&pagesize=1&order=desc&sort=votes&filter=!-*jbN.L_SaIL'
